10292d9a969559323ec9827af3143315e9c70954
Luken <lcamarero@birt.eus>
2024-04-26 15:11:00 +0200

    Versión 1. Commit con cambios básicos

--- src/main/java/luken/ud04ejemplogit/UD04EjemploGit.java
+++ src/main/java/luken/ud04ejemplogit/UD04EjemploGit.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package luken.ud04ejemplogit;
 
 /**
  *
- * @author Caja
+ * @author: Luken
  */
 public class UD04EjemploGit {
 
     public static void main(String[] args) {
-        
+        System.out.println("Ejemplo de Git");
     }
 }
